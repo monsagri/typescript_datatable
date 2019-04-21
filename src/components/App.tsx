b5d5b9b2155c84ee55ed4f5c927882d0dd1fde25
Fabian Feldberg <fabian@jdlt.co.uk>
2019-04-21 18:44:24 +0100

    add basic sorting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,29 @@ import React from "react";
 import { Container, Header, Segment } from "semantic-ui-react";
 import DataTable from "./DataTable";
 
-const tableTest = [
+const data = [
   { name: "test", id: 1, price: 15, company: "KFD" },
   { name: "burger", id: 2, price: 2, company: "MCD" },
   { name: "burrito", id: 3, price: 7, company: "ABSURD BIRD" }
+];
+
+const columns = [
+  {
+    dataIndex: "name",
+    label: "Name"
+  },
+  {
+    dataIndex: "id",
+    label: "Id"
+  },
+  {
+    dataIndex: "price",
+    label: "Price"
+  },
+  {
+    dataIndex: "company",
+    label: "Company"
+  }
 ];
 
 const App = (props: any) => {
@@ -15,7 +34,12 @@ const App = (props: any) => {
       <Header as="h1" content="Hello Typescript" />
       <Segment>Hi</Segment>
       <Segment>
-        <DataTable data={tableTest} keyField="name"/>
+        <DataTable
+          data={data}
+          keyField="name"
+          columns={columns}
+          sorting={{ defaultSortBy: "name", defaultSortAscending: true }}
+        />
       </Segment>
     </Container>
   );
--- src/components/DataTable/index.tsx
+++ src/components/DataTable/index.tsx
@@ -1,41 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "semantic-ui-react";
 
+interface Map {
+  [key: string]: any;
+}
 interface SortingProps {
-  defaultSortBy: string,
-  defaultSortAscending?: boolean
+  defaultSortBy: string;
+  defaultSortAscending?: boolean;
 }
 
 interface DataTableColumn {
-  dataIndex: string,
-  label?: string,
-  render?: (props: any) => any,
-  type?: (props: any) => any,
-
+  dataIndex: string;
+  label?: string;
+  render?: (props: any) => any;
+  type?: (props: any) => any;
 }
 
 interface DataTableProps {
-  data: Array<object>;
+  data: Array<Map>;
   keyField: string;
-  sorting?: SortingProps,
-  columns?: Array<DataTableColumn>
+  sorting?: SortingProps;
+  columns?: Array<DataTableColumn>;
 }
 
 const DataTable = (props: DataTableProps) => {
-  const { columns } = props
+  // Prop recovery
+  let { data } = props;
+  const { columns, sorting } = props;
+
+  // Sorting related code
+  const [sortColumn, setSortColumn] = useState<string>("");
+  const [sortDirection, setSortDirection] = useState<
+    "ascending" | "descending" | undefined
+  >(sorting && sorting.defaultSortAscending ? "ascending" : "descending");
+
+  data = sorting // This should care about default sort direction and defaultSortBy being defined
+    ? data.sort(
+        (a: Map, b: Map) => a[sorting.defaultSortBy] - b[sorting.defaultSortBy]
+      )
+    : data;
+
+  const handleSort = (clickedColumn: string) => () => {
+    if (clickedColumn !== sortColumn) {
+      setSortColumn(clickedColumn);
+      setSortDirection("ascending");
+      data = data.sort((a: Map, b: Map) => a[clickedColumn] - b[clickedColumn]);
+    }
+    setSortDirection(
+      sortDirection === "ascending" ? "descending" : "ascending"
+    );
+    data = data.reverse();
+  };
+
+  // Render
   return (
-    <Table>
+    <Table sortable>
       <Table.Header>
         <Table.Row>
-          {columns && columns.map((column: DataTableColumn) => (
-            <Table.HeaderCell>{column.label}</Table.HeaderCell>
-          ))}
+          {columns &&
+            columns.map((column: DataTableColumn) => (
+              <Table.HeaderCell
+                key={column.label}
+                sorted={column.label === sortColumn ? sortDirection : undefined}
+                onClick={sorting ? handleSort(column.dataIndex) : undefined}
+              >
+                {column.label}
+              </Table.HeaderCell>
+            ))}
         </Table.Row>
       </Table.Header>
       <Table.Body>
-        <Table.Row>
-          <Table.Cell>Test</Table.Cell>
-        </Table.Row>
+        {data.map((datum: Map) => (
+          <Table.Row key={data.indexOf(datum)}>
+            {columns &&
+              columns.map((column: DataTableColumn) => (
+                <Table.Cell key={column.dataIndex}>
+                  {datum[column.dataIndex]}
+                </Table.Cell>
+              ))}
+          </Table.Row>
+        ))}
       </Table.Body>
     </Table>
   );
